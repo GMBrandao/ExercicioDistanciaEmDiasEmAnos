a1d07813b051f482714136b7895cec5a34ea2f1d
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-06 01:10:09 -0300

    Add project files.

---
+++ DistanciaEmDiasEmAnos/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int firstYear, finalYear, count, days = 0;
+
+        Console.WriteLine("Digite qual o ano inicial:");
+        firstYear = int.Parse(Console.ReadLine());
+
+        do
+        {
+            Console.WriteLine("Digite qual o ano final:");
+            finalYear = int.Parse(Console.ReadLine());
+            if (finalYear < firstYear)
+                Console.WriteLine("Número inválido\nDigite um ano maior que o ano inicial");
+        } while(finalYear < firstYear);
+
+        for (count = firstYear; count < finalYear + 1; count++)
+        {
+            if ((count % 4 == 0) && (count % 100 != 0) || (count % 400 == 0))
+                days += 366;
+            else
+                days += 365;
+        }
+
+        Console.WriteLine("\nDo início do ano " + firstYear + " até o final do ano "
+            + finalYear + " tem " + days + " dias.");
+    }
+}
